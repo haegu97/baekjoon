84476c00b45d686e1be22a764f0935b90d217da4
Sungu Kim <108677235+haegu97@users.noreply.github.com>
2024-01-22 15:32:59 +0900

    240122

---
+++ 15649.py
@@ -0,0 +1,17 @@
+n, m = map(int, input().split())
+arr = []
+visited = [False] * (n+1)
+
+def dfs(num):
+    if num == m:
+        print(' '.join(map(str, arr)))
+        return
+    for i in range(1, n+1):
+        if visited[i] == False:
+            visited[i] = True
+            arr.append(i)
+            dfs(num+1)
+            visited[i] = False
+            arr.pop()
+
+dfs(0)
